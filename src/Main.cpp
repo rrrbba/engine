fca126e02631a9331223080e321fcd623698aa12
rrrbba <rrrbba@rayven.com>
2024-01-28 14:21:46 -0500

    added sdl info

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
-int main() {
+
+int main(int argc,char* argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING); //initialize everything
+
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
+
+// g++ -Wall -std=c++17 Main.cpp -o Main -I/opt/homebrew/include -L/opt/homebrew/lib -lSDL2 -lm
