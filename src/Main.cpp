2135901014fd84ca3a7efd89a6bc0a2350467809
rrrbba <rrrbba@rayven.com>
2024-01-28 22:29:36 -0500

    added dependencies

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,9 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
 
 int main(int argc,char* argv[]) {
